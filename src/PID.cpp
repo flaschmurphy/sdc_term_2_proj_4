bc2c5af95008aaae0d422250c2852b5d902a0ce6
Ciaran Murphy <ciaranrmurphy@gmail.com>
2018-03-17 13:15:49 +0100

    Initial commit with sub-optimal params

---
+++ src/PID.cpp
@@ -0,0 +1,43 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) 
+{
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+
+    is_initialized = true;
+}
+
+void PID::UpdateError(double cte) {
+
+    // Update the differential error
+    d_error = cte - p_error;
+
+    // Update the proportional error
+    p_error = cte;
+
+    // Update the integral error
+    i_error += cte;
+
+}
+
+double PID::TotalError() {
+    return Kp*p_error + Kd*d_error + Ki*i_error;
+}
+
+
